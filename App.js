39b8a1d783c6b5985aaab9d6acca2a16a1984400
roganantonys <roganantonys.ug20.ec@francisxavier.ac.in>
2024-01-17 12:18:52 +0530

    first commit

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import SignInScreen from "./sourceCode/screens/signInScreens/SignInScreen.js";
-import SignUpScreen from "./sourceCode/screens/SignUpScreen.js";
-import ConfirmEmailScreen from "./sourceCode/screens/ConfirmEmailScreen.js";
-import PasswordResetScreen from "./sourceCode/screens/PasswordResetScreen.js";
+
 import Navigations from "./sourceCode/Navigations.js";
 
 export default function App() {
